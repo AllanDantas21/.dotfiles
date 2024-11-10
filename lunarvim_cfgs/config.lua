f394925ba3db60d3d2fceeb6882aa7162202a982
allandantas21 <allandantas21.ad@gmail.com>
2024-11-10 18:52:14 -0300

    add lunarvim_cfg

---
+++ lunarvim_cfgs/config.lua
@@ -0,0 +1,36 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+-- keymapsi
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+
+-- disable C lsp
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "clangd" })
+
+-- set timeout
+vim.opt.timeoutlen = 500
+
+lvim.plugins = {
+    -- Plugin para formatação de código 42
+    { 'cacharle/c_formatter_42.vim' },
+
+    -- Plugin 42-header
+    {
+        "Diogo-ss/42-header.nvim",
+        lazy = false,  -- Carrega o plugin na inicialização
+        config = function()
+            local header = require("42header")
+            header.setup({
+                default_map = true,   -- Mapeamento padrão <F1> no modo normal
+                auto_update = true,   -- Atualiza o cabeçalho ao salvar
+                user = "aldantas",    -- Seu usuário
+                mail = "aldantas@student.42.rio",  -- Seu e-mail
+            })
+        end
+    },
+}
