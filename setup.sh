5122881d0baf7257d0fb27406959df859cca7879
allandantas21 <allandantas21.ad@gmail.com>
2025-02-01 18:23:49 -0300

    add first step in setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+# get the windows username
+WIN_USER=WIN-USER
+
 # script to setup the environment for development
+
+# create the destination directory, if it doesn't exist
+mkdir -p /mnt/c/Users/$WIN_USER/appdata/roaming/alacritty
+
+# setup alacritty configs
+ln -s ~/dotfiles/alacritty/alacritty.toml /mnt/c/Users/$WIN_USER/appdata/roaming/alacritty/alacritty.toml
